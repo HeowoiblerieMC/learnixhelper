0eada81985a68be6c508538403e6c2c57cf794c9
HeowoiblerieMC <hajimeu0617@icloud.com>
2026-09-20 00:25:54 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,3 @@
 function sendMessage() {
-
-    const input = document.getElementById("user-input");
-    const chatBox = document.getElementById("chat-box");
-
-    const text = input.value.trim();
-
-    if (!text) return;
-
-    chatBox.innerHTML += `
-        <div class="message">
-            <span class="user">You:</span> ${text}
-        </div>
-    `;
-
-    let response = "Hello! I am LearnixAI.";
-
-    if (/[ぁ-んァ-ン一-龥]/.test(text)) {
-        response = "こんにちは！私は LearnixAI です！";
-    }
-
-    else if (
-        /[àâçéèêëîïôûùüÿœ]/i.test(text) ||
-        /(bonjour|salut|merci)/i.test(text)
-    ) {
-        response = "Bonjour ! Je suis LearnixAI.";
-    }
-
-    chatBox.innerHTML += `
-        <div class="message">
-            <span class="ai">LearnixAI:</span> ${response}
-        </div>
-    `;
-
-    input.value = "";
-
-    chatBox.scrollTop = chatBox.scrollHeight;
+    alert("LearnixAI is working!");
 }
